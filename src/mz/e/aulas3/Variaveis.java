937ce09068216b110ebba31af9b4bcb2d696df6f
aly <alymacome@gmail.com>
2019-12-04 22:21:30 +0200

    Introducao a variaveis

--- src/mz/e/aulas3/Variaveis.java
+++ src/mz/e/aulas3/Variaveis.java
@@ -11,4 +11,22 @@ package mz.e.aulas3;
  */
 public class Variaveis {
     
+    public static void main(String[]args){
+        
+        //variaveis inteira
+        int idade = 10;
+        int tamanho;
+        
+        //variaveis double ou decimais
+        double preco = 9.5;
+        double temperatura;
+        
+        //variaveis que contem apenas um caracter nota que o caracter deve estar sempre dentro de aspas simples
+        char sexo = 'M';
+        
+        //vareiavel que contem uma cadeia de caracteres
+        String sexoStr = "Masculino";
+    
+    }
+    
 }
