f03bf3b4ab42126bd243b2dae13223d6dfe1dfab
aly <alymacome@gmail.com>
2019-12-15 19:40:16 +0200

    Aula operadores

--- src/mz/e/aulas3/Operadores.java
+++ src/mz/e/aulas3/Operadores.java
@@ -16,27 +16,31 @@ package mz.e.aulas3;
  * @author Valter Massango
  * @author Efraime Mulote
  */
+
+/**
+ * 
+ * existem varios tipos de operadores
+ * operadores artimetricos;
+ * operadores logicos;
+ * operadores de atribuicao;
+ * operadores de incremento e decremento;
+ * operadores de igualdade;
+ * operadores relacionais
+ * 
+ * 
+ */
 public class Operadores {
     
+        
+    //operador de atribuicao (=) e usadado para definir ou atribuir um valor a uma variavel.
+    int idade = 20;
+    double altura = 1.5; 
+    
+    //operadores artimetricos realisam operacoes basicas da matematica e retornam o resultado
+    int area = 2*4;
+            
     public static void main (String[]args){
-        
-        //variaveis inteiras
-        int idade = 10;
-        int tamanho = 9;
-        
-        //variaveis double ou decimais
-        double preco = 10.5;
-        double temperatura;
-        
-        // Avariavel que contem apenas um caracter e deve estar entre aspas simples
-        char sexo = 'M';
-        
-        //Declaracao da variaveis string
-        String sexoStr = "Masculino";
-        
-        
-        
-        
+             
     }
     
 }
--- src/mz/e/aulas3/Variaveis.java
+++ src/mz/e/aulas3/Variaveis.java
@@ -8,11 +8,32 @@ package mz.e.aulas3;
 /**
  *
  * @author Aly Macome
+ * @author Jose Seie
+ * @author Filipe Mangue
+ * @author Jose Romao
+ * @author Abdul Rasac
+ * @author Adriano Ueca
+ * @author Valter Massango
+ * @author Efraime Mulote
+ */
+
+/**
+ * o que sao variaveis?
+ * Variável é uma região de memória reservada para armazenar valores de modo temporário.
  */
 public class Variaveis {
+    //VARIAVEIS DE INSTANCIA OU DE OBJECTOS
+    String nome;
+    double altura;
+    double peso;
+    
+    //variaveis estaticas
+    static int numero;
+    static double preco;
+    static char sexo;
     
     public static void main(String[]args){
-        
+        //EXEMPLO DE VARIAVEIS LOCAIS
         //variaveis inteira
         int idade = 10;
         int tamanho;
@@ -26,6 +47,9 @@ public class Variaveis {
         
         //vareiavel que contem uma cadeia de caracteres
         String sexoStr = "Masculino";
+        
+        //
+        
     
     }
     
